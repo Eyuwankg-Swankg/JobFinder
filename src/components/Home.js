56dff56f3b6fbddca99fe4deb484fc2dfa5f2067
Myname <myemail@email.com>
2020-09-08 10:26:28 +0530

    Completed Landing Page with Responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import { Link} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import Job from "../img/2.jpg";
 const Home = () => {
-    return (
+  const [signin, setSignin] = useState(false);
+  const [signup, setSignup] = useState(false);
+  if (signup) {
+    return <Redirect to="/signup" />;
+  }
+  if (signin) {
+    return <Redirect to="/signin" />;
+  }
+  return (
+    <div id="home-container">
+      <nav id="home-navbar">
+        <h1>Jobconnector</h1>
+        <ul>
+          <li onClick={() => setSignin(true)}>Sign In</li>
+          <li onClick={() => setSignup(true)}>Sign Up</li>
+        </ul>
+      </nav>
+      <div id="home-area">
+        <img src={Job} />
         <div>
-            <h1>Eyuwankg</h1>
-            <Link to="/signin">Sign In</Link>
-            <Link to="/profile">Profile</Link>
-            <Link to="/signup">Sign Up</Link>
+          <h1>Welcome To JobConnector !!!</h1>
+          <h5>
+            Are you eagerly searching for a job? Find your dream job here!!!
+          </h5>
+          <h4>
+            “It doesn’t matter whether you do a small or big job, What matters
+            is job contentment”
+          </h4>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
+
+/* <Link to="/signin">Sign In</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/signup">Sign Up</Link> */
