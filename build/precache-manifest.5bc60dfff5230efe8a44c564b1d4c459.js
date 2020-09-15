5d8a07c4c056c66c1f2cd22577dd4a301154e764
Eyuwankg-Swankg <messikarthik13@gmail.com>
2020-09-15 14:25:17 +0530

    Production Build

---
+++ build/precache-manifest.5bc60dfff5230efe8a44c564b1d4c459.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd30a80cff0411abd6931566ca411d1",
+    "url": "/JobFinder/index.html"
+  },
+  {
+    "revision": "dec243f09e2069a2bd33",
+    "url": "/JobFinder/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "625a3cd6609143c8b75a",
+    "url": "/JobFinder/static/css/main.39e78ffc.chunk.css"
+  },
+  {
+    "revision": "dec243f09e2069a2bd33",
+    "url": "/JobFinder/static/js/2.77669f2f.chunk.js"
+  },
+  {
+    "revision": "414115f77820353bb73df8d3b7df13d8",
+    "url": "/JobFinder/static/js/2.77669f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625a3cd6609143c8b75a",
+    "url": "/JobFinder/static/js/main.5674c353.chunk.js"
+  },
+  {
+    "revision": "6cb0897e6b8662663263",
+    "url": "/JobFinder/static/js/runtime-main.4c341081.js"
+  },
+  {
+    "revision": "660915bc24f289bbf5a0986b7ce42b4f",
+    "url": "/JobFinder/static/media/send.660915bc.svg"
+  }
+]);
